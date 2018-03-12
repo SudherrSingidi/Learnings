46c69568748807e18140d14864bf17dd84f9af25
Sudheer Reddy Singidi <sudheerreddysingidi@Sudheers-MacBook-Pro.local>
2018-03-11 23:16:21 -0500

    python

--- python/rps.py
+++ python/rps.py
@@ -23,36 +23,77 @@
 # 		print("User 1 wins!")
 # 	else:
 # 		print("user 2 wins")
-	
+import getpass
+print("Welcome to:\n....Rock....\n.....Paper.....\n......Scissors......\nYou are about to play")
+while True:
+	try:
+		number_of_games = int(input("How many times you would like to play RPS in this game: "))
+	except ValueError:
+		print("I did not understand that.Please enter a valid number")
+		continue
+	else:
+		break
+if number_of_games >= 0:
+	print(f"You chose to play RPS {number_of_games} times")
+choice = input(f"Would you like to continue (y/n) ").upper()
+user_1 = 0
+user_2 = 0	
+while choice == 'Y':
+	print("Game starts now. Good luck!!!")
+	for r in range(number_of_games):
+		input_1 = getpass.getpass("Enter option for user1: ")
+		input_1 = input_1.lower()
+		# check for valid input from user1
+		if input_1 != "scissors" and input_1 != "rock" and input_1 != "paper":
+			# If input is invalid,ask user input until the input is valid
+			while  input_1 != "scissors" or input_1 != "rock" or input_1 != "paper":
+				print("Dear user1,your input is invalid.please enter valid input")
+				input_1 = getpass.getpass("Enter option for user1: ")
+				if input_1 == "scissors" or input_1 == "rock" or input_1 == "paper":
+					break
+		input_2 = getpass.getpass("Enter option for user2: ")
+		input_2 = input_2.lower()
 
-input_1 = input("Enter option for user1: ")
-input_1 = input_1.lower()
-# check for valid input from user1
-if input_1 != "scissors" and input_1 != "rock" and input_1 != "paper":
-	# If input is invalid,ask user input until the input is valid
-	while  input_1 != "scissors" or input_1 != "rock" or input_1 != "paper":
-		print("Dear user1,your input is invalid.please enter valid input")
-		input_1 = input("Enter option for user1: ")
-		if input_1 == "scissors" or input_1 == "rock" or input_1 == "paper":
-			break
-input_2 = input("Enter option for user2: ")
-input_2 = input_2.lower()
+		# check for valid input from user2
+		if input_2 != "scissors" and input_2 != "rock" and input_2 != "paper":
+			# If input is invalid,ask user input until the input is valid
+			while  input_2 != "scissors" or input_2 != "rock" or input_2 != "paper":
+				print("Dear user2,your input is invalid.please enter valid input")
+				input_2 = getpass.getpass("Enter option for user2: ")
+				if input_2 == "scissors" or input_2 == "rock" or input_2 == "paper":
+					break
+		# check the winner	
+		if (input_1 == "scissors" and input_2 == "paper") or (input_1 == "paper" and input_2 == "rock") or (input_1 == "rock" and input_2 == "scissors"):
+			print("User 1 wins!")
+			user_1 += 1
 
-# check for valid input from user2
-if input_1 != "scissors" and input_1 != "rock" and input_1 != "paper":
-	# If input is invalid,ask user input until the input is valid
-	while  input_2 != "scissors" or input_2 != "rock" or input_2 != "paper":
-		print("Dear user2,your input is invalid.please enter valid input")
-		input_2 = input("Enter option for user2: ")
-		if input_2 == "scissors" or input_2 == "rock" or input_2 == "paper":
-			break
-# check the winner	
-if (input_1 == "scissors" and input_2 == "paper") or (input_1 == "paper" and input_2 == "rock") or (input_1 == "rock" and input_2 == "scissors"):
-	print("User 1 wins!")
-elif (input_2 == "scissors" and input_1 == "paper") or (input_2 == "paper" and input_1 == "rock") or (input_2 == "rock" and input_1 == "scissors"):
-	print("user 2 wins") 
-else:
-	print("Tied") 
+		elif (input_2 == "scissors" and input_1 == "paper") or (input_2 == "paper" and input_1 == "rock") or (input_2 == "rock" and input_1 == "scissors"):
+			print("user 2 wins") 
+			user_2 += 1
+		else:
+			print("Tied") 
+	if user_2 > user_1:
+		print(f"User 2 won {user_2} times.So, User 2 wins")				
+	elif user_2 < user_1:
+		print(f"User 1 won {user_1} times.So, User 1 wins")
+	elif user_1 == user_2:
+		print("It's a tie")
+	choice = input("Would you like to play another game? (y/n) ").upper()
+	if choice == 'Y':
+		while True:
+			try:
+				number_of_games = int(input("How many times you would like to play RPS in this game: "))
+			except ValueError:
+				print("I did not understand that.Please enter a valid number")
+				continue
+			else:
+				break
+		if number_of_games >= 0:
+			print(f"You chose to play RPS {number_of_games} times")
+			user_1 = 0
+			user_2 = 0	
+print("See you next time!.")
+
 
 
 
