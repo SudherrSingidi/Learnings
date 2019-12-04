076408f76be457bdd764e53282071998677a6c1a
Sudheer Singidi <ssingidi@umn.edu>
2019-12-03 20:14:09 -0600

    completed version

--- python/GuessingGameOne.py
+++ python/GuessingGameOne.py
@@ -1,8 +1,9 @@
 import random
 a = random.randint(0,9)
-print(a)
 user_count = 0
 system_count = 0
+no_of_correct_guesses = 0
+number_of_guesses = 0
 choice = 'Y'
 while True:
     try:
@@ -13,35 +14,33 @@ while True:
     else:
         break
 while choice == 'Y':
+    a = random.randint(0,9)
     if user_input < a :
         print("Your guess is too low!")
-        system_count = system_count + 1
-        print(choice)
     elif user_input > a :
         print("Your guess is too high")
-        system_count = system_count + 1
     elif user_input == a :
-        user_count = user_count + 1
+        no_of_correct_guesses = no_of_correct_guesses + 1
         print(f"Congratulations!! Your guess: {user_input} matches with random number which is: {a}")
-        choice = ' '
-		while choice != 'Y' or choice != 'N':
-			choice = input("Would you like to play another game? (y/n) ").upper()
-			if choice == 'Y' or choice == 'N':
-                break
-            if choice != 'Y' or choice != 'N':
-                print("I did not understand that.Please enter a valid option")
+        continue
+    choice = ' '
+    while choice != 'Y' or choice != 'N':
+        choice = input("Would you like to play another game? (y/n) ").upper()
+        if choice == 'Y' or choice == 'N':
+            number_of_guesses = number_of_guesses + 1
+            break
+        if choice != 'Y' or choice != 'N':
+            print("I did not understand that.Please enter a valid option")
     if choice == 'Y':
         while True :
-	        try :
+            try :
                 user_input = int(input("what is your guess : "))
             except ValueError:
                 print("I did not understand that.Please enter a valid number")
                 continue
             else :
                 break
-        if number_of_games >= 0:
-			user_count = 0
-			system_count = 0
-print(user_input ,a)
-print(system_count,user_count)
+print(f"Total number of guesses attempted by user : {number_of_guesses}")
+print(f"Total number of successfull guesses : {no_of_correct_guesses}")
+print("see you next time!")
 
